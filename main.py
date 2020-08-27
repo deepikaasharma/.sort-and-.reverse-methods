07d9795285342aa9a946542f546ac5921e5df23f
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-27 09:13:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+numbers = [3, 2, 1, 6, 4]
+numbers.sort()
+
+print(numbers)
+
+a = 10
+n = 20
+
+num_list = list(range(a, n))
+
+num_list.reverse()
+print(num_list)
+
+
+numbers = [1, 2, 3, 4, 5]
+numbers.reverse()
+
+print(numbers)
+
+
+fruit_list = ['apple', 'pear', 'peach', 'mango']
+
+fruit_list.sort()
+
+print(fruit_list)
+
+
+
+
+
